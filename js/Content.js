f645c96d6e87496a04ec4e422ea12f1727840e16
st688 <st688@cornell.edu>
2014-05-21 16:03:13 -0400

    test

--- js/Content.js
+++ js/Content.js
@@ -2,7 +2,7 @@
 function loadContent(page) {
    $.get("pages/" + page + ".html", function(data) {
       $('#Content').html(data);
-      $('#Page_css').attr("href", "css/" + page + ".css");
+      $('#Page_css').href = "css/" + page + ".css";
    });
    return false;
 }
