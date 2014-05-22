3779cd057d951eab551638b04639b5f0eaa3d6e6
st688 <st688@cornell.edu>
2014-05-22 10:58:50 -0400

    rewrite links by js

---
+++ js/link.js
@@ -0,0 +1,23 @@
+function loadLinks() {
+   $.get("pages/staff/data", function(data) {
+      // 預備輸出的html
+      var html_stack = "";
+      $('l',data).each(function(){
+         var t, a;
+
+         t = $(this).find('t').text(); // 標題
+         a = $(this).find('a').text(); // 網址
+
+         // 生成連結
+         html_stack = html_stack 
+                    + "<div class='link'>\n"
+                    + "   <div class='title'>" + t + "</div>\n"
+                    + "   <div class='href'><a href=\"" + a + "\">" + a + "</a></div>\n"
+                    + "</div>\n";
+
+      });
+      $('#Links').html(html_stack);
+   });
+}
+
+addLoadEvent(loadLinks());
