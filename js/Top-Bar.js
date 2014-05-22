fad68c88242c934b7322bacb9cbfeabfae2f137f
st688 <st688@cornell.edu>
2014-05-22 15:53:04 -0400

    test

--- js/Top-Bar.js
+++ js/Top-Bar.js
@@ -1,15 +1,13 @@
 // 讓導覽列往下捲之後定在最上方的程式碼
 $(function(){
-   $(window).load(function(){
-      $(window).bind('scroll', function(){
-         //當高度大於150時，Top Bar fixed at top 
-         if($(this).scrollTop() < 150){
-            $('#Top-Bar').css('position','static')
-                         .css('top', 'auto' );
-         }else{
-            $('#Top-Bar').css('position','fixed')
-                         .css('top', 0 );
-         }
-      });
+   $(window).bind('scroll', function(){
+      //當高度大於150時，Top Bar fixed at top 
+      if($(this).scrollTop() < 150){
+         $('#Top-Bar').css('position','static')
+                      .css('top', 'auto' );
+      }else{
+         $('#Top-Bar').css('position','fixed')
+                      .css('top', 0 );
+      }
    });
 });
