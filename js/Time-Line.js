822b1de41c2c91c9a2d0519d4dad2dfd60f8fdd1
st688 <st688@cornell.edu>
2014-05-24 16:00:19 -0400

    test

--- js/Time-Line.js
+++ js/Time-Line.js
@@ -93,7 +93,7 @@ var dx_feasible_max = 0; // 最右可以拉到哪裡
 // 滑鼠按下
 function fMouseDown(e){
    clientX_down         = e.clientX;
-   slider_x             = $("#Slider").getBoundingClientRect().left;
+   slider_x             = document.getElementsByTagName("#Slider").attr('left');
 
 alert( clientX_down + " " + slider_x);
    switch( time_line_style ){
