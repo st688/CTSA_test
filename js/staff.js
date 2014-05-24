0803feea6b949422a321cd7c366d57b51b197c60
st688 <st688@cornell.edu>
2014-05-24 00:01:55 -0400

    test

--- js/staff.js
+++ js/staff.js
@@ -45,9 +45,7 @@ function loadStaff(yeartag) {
 
 function initStaff(){
 //   var latest, decade, year_min, year_max;
-alert("a1");
-   initTimeLine('staff');
-
+   initTimeLine("staff");
 /*
    $.get("data/staff/years", function(data) {
       // 取得最新年份
@@ -73,5 +71,5 @@ alert("a1");
    });
 */
 }
-setttt();
+
 addLoadEvent(initStaff());
