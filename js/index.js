76195670de87e3d7b0ab032697697d66940fda67
st688 <st688@cornell.edu>
2014-05-24 21:14:11 -0400

    test

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 var pic = 2; // 首頁隨機圖片數
 
-function initMainPicture(){
-   var html_stack = "<img src=\"images/index/main" + Math.floor(Math.random()*pic) + ".jpg\" />";
-   $('#MainPic').html(html_stack);
-}
+//function initMainPicture(){
+   $('#MainPic').html("<img src=\"images/index/main" + Math.floor(Math.random()*pic) + ".jpg\" />");
+//}
 
-addLoadEvent(initMainPicture());
+//addLoadEvent(initMainPicture());
