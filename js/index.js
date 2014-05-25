f7e8e3b80c0394399b12990437d2ea6fef50398c
st688 <st688@cornell.edu>
2014-05-24 21:12:55 -0400

    test

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ var pic = 2; // 首頁隨機圖片數
 
 function initMainPicture(){
    var html_stack = "<img src=\"images/index/main" + Math.floor(Math.random()*pic) + ".jpg\" />";
-alert(html_stack);
    $('#MainPic').html(html_stack);
 }
 
