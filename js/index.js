0508601e9947fe8396d4bb5529cf051dfddda839
st688 <st688@cornell.edu>
2014-05-24 22:05:19 -0400

    fix content

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
 var pic = 2; // 首頁隨機圖片數
 
-document.write ("<img src=\"images/index/main" + Math.floor(Math.random()*pic) + ".jpg\" />");
+document.write ("<div id='MainPic'><img src=\"images/index/main" + Math.floor(Math.random()*pic) + ".jpg\" /></div>");
