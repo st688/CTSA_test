febdfe42c00afaa8c484185aa1bc7f9401600034
st688 <st688@cornell.edu>
2014-05-26 16:45:05 -0400

    add subscribe

---
+++ js/Pop-Screen.js
@@ -0,0 +1,15 @@
+// 彈出視窗，把指定檔案內容讀入
+function popScreen(page_path){
+   $.get(page_path, function(data) {
+      $("#Pop-Screen-Content").html(data);
+   });
+
+   $("#Pop-Screen").css('display','block');
+   return false;
+}
+
+// 關閉視窗
+function closeScreen(){
+   $("#Pop-Screen").css('display','none');
+   return false;
+}
