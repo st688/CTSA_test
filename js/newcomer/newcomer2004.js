ad902ab60827f00efe63eeaaef11ddf63af4ba3b
st688 <st688@cornell.edu>
2014-06-06 23:34:26 -0400

    newstudent

---
+++ js/newcomer/newcomer2004.js
@@ -0,0 +1,10 @@
+function loadNewcomer2004(page) {
+   $('#Newcomer-Menu-List li').css('color','#cc0000');
+   $('#Option'+page+' p').css('color','#003399');
+   $.get("data/newcomer/n2004/page" + page, function(data) {
+      $('#Newcomer-Container').html(data);
+   }
+}
+
+
+addLoadEvent(loadNewcomer2004('1'));
