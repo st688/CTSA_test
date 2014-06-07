e89bc862ad7cbc76d44ef8fcfc53e5070455f8a1
st688 <st688@cornell.edu>
2014-06-07 17:34:23 -0400

    ann

--- js/index.js
+++ js/index.js
@@ -9,7 +9,6 @@ var pic = 4; // 首頁隨機圖片數
 
 // 讓內文頂部與底部無距離
 $('.Content-Fix').css('height','0px');
-document.write ("<div id='MainPic'><img src=\"images/index/main" + Math.floor(Math.random()*pic) + ".jpg\" />" + 
-"<div id='Announcement'></div></div>");
+document.write ("<img src=\"images/index/main" + Math.floor(Math.random()*pic) + ".jpg\" />");
 
 addLoadEvent(initAnnouncement());
