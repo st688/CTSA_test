3e25f72cb17f8ab551446d814aa98d3a1607acbb
st688 <st688@cornell.edu>
2014-06-06 23:41:41 -0400

    fix n2004.js

--- js/newcomer/newcomer2004.js
+++ js/newcomer/newcomer2004.js
@@ -1,6 +1,6 @@
 function loadNewcomer2004(page) {
-   $('#Newcomer-Menu-List li').css('color','#cc0000');
-   $('#Option'+page+' p').css('color','#003399');
+   $('#Newcomer-Menu-List li p').css('color','#003399');
+   $('#Option'+page+' p').css('color','#cc0000');
    $.get("data/newcomer/n2004/page" + page, function(data) {
       $('#Newcomer-Container').html(data);
    });
