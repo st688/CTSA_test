d153673edd044c9be8dc5b6c8299be7396e572ac
st688 <st688@cornell.edu>
2014-06-07 17:27:37 -0400

    announcement css test

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+function initAnnouncement(){
+   $.get("data/index/announcement", function(data) {
+      $("#Announcement").html(data);
+   });
+   return false;
+}
+
 var pic = 4; // 首頁隨機圖片數
 
 // 讓內文頂部與底部無距離
 $('.Content-Fix').css('height','0px');
 document.write ("<div id='MainPic'><img src=\"images/index/main" + Math.floor(Math.random()*pic) + ".jpg\" />" + 
 "<div id='Announcement'></div></div>");
+
+addLoadEvent(initAnnouncement());
