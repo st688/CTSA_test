4e984dabb7bde3078daed638477955c37e8724aa
st688 <st688@cornell.edu>
2014-06-08 21:55:49 -0400

    news structure test

--- js/news.js
+++ js/news.js
@@ -11,19 +11,19 @@ function loadNewsList() {
          a = $(this).find('a').text(); // 發佈人
          f = $(this).find('f').text(); // 對應檔案，檔名為 "s" + 月 + 日 + "_" + f
          time_mdy = m.split("/");      // 月,日,年
-         is_file  = ();
+
          // 生成連結
          html_stack += "<div class='news'>\n" 
                      + "<a href=\"#\" onclick='return ";
          // 測試是否為一般檔案
-         if( f.indexOf(".")>-1 ){
+         if( f.indexOf(".") > -1 ){
             // 若是，提供下載
             html_stack += "loadNewScreen";
          }else{
             // 若否，彈出視窗
             html_stack += "popScreen";
          }
-         html_stack += "(\"data/news/y" + time_mdy[2] + "/s" + time_mdy[0] + time_mdy[1] + "_" + f + "\");'>";                     
+         html_stack += "(\"data/news/y" + time_mdy[2] + "/s" + time_mdy[0] + time_mdy[1] + "_" + f + "\");'>"                    
                      + "   <div class='news_time'>"      + m + "</div>\n"
                      + "   <div class='news_title'>"     + t + "</div>\n"
                      + "   <div class='news_announcer'>" + a + "</div>\n"
