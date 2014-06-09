4c2d9ee6fa530d2bfc053e509fe10bbd46c37549
st688 <st688@cornell.edu>
2014-06-08 22:22:35 -0400

    link layer test

--- js/links.js
+++ js/links.js
@@ -2,18 +2,25 @@ function loadLinks() {
    $.get("data/links/data", function(data) {
       // 預備輸出的html
       var html_stack = "";
-      $('l',data).each(function(){
-         var t, a;
-
+      $('g',data).each(function(){
          t = $(this).find('t').text(); // 標題
-         a = $(this).find('a').text(); // 網址
+         html_stack += "<div class='Link-Group'>\n"
+                     + "<div class='Link-Group-Title'>" + t + "</div>\n";
 
-         // 生成連結
-         html_stack += "<div class='link'>\n"
-                     + "   <div class='title'>" + t + "</div>\n"
-                     + "   <div class='href'><a href=\"#\" onclick=\"return loadNewScreen('http://" + a + "');\">" + a + "</a></div>\n"
-                     + "</div>\n";
+         $('l',$(this)).each(function(){
+            var t, a;
 
+            t = $(this).find('t').text(); // 標題
+            a = $(this).find('a').text(); // 網址
+
+            // 生成連結
+            html_stack += "<div class='link'>\n"
+                        + "   <div class='title'>" + t + "</div>\n"
+                        + "   <div class='href'><a href=\"#\" onclick=\"return loadNewScreen('http://" + a + "');\">" + a + "</a></div>\n"
+                        + "</div>\n";
+
+         });
+         html_stack += "</div>\n";
       });
       $('#Links').html(html_stack);
    });
