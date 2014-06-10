84337cfdd67c719a1b053132f2bb5c800e0e9a9b
st688 <st688@cornell.edu>
2014-06-09 22:07:48 -0400

    link test

--- js/links.js
+++ js/links.js
@@ -36,7 +36,7 @@ function loadLinks() {
             offset -= $(this).height();
          }
       });
-      $('.Link-Group').css('width', $('#Links').width()/2 - 5);
+      $('.Link-Group').css('width', $('#Links').width()/2 - 10);
    });
 }
 
