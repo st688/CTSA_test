20261690d5356764952b629b629d647155791eec
st688 <st688@cornell.edu>
2014-06-26 21:40:03 -0400

    add Web-Jump

---
+++ js/tools/Web-Jump.js
@@ -0,0 +1,16 @@
+// 全域變數，表示首頁已讀入
+var __INDEX__ = true;
+
+// 從外部來的連結
+var hash = location.hash;
+if(hash.length > 1){
+   hash = hash.substring(1);
+   if(hash[0] == '?'){
+      // #? 彈出視窗
+      popScreen(hash.substring(1));
+   }else{
+      // # 切換內文
+      loadContent(hash);
+   }
+   location.hash = '';
+}
